ea036e34566ad2fd15f592e54a08c983e8e6159a
James <james@jarofgreen.co.uk>
2026-02-12 10:31:12 +0000

    Python 3.13, Host on Debian 13, add gunicorn, remove hosting docs for now

--- docs/for_sysadmins/index.rst
+++ docs/for_sysadmins/index.rst
@@ -9,5 +9,4 @@ If you want to install and host DataTig Hub on your own server, this section is 
 
    settings.rst
    github-oauth-apps.rst
-   host_on_vm/index.rst
    monitor.rst
--- host_on_vm/backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-current_date=$(date +"%d")
-
-pg_dump --host=localhost --username=datatighub --no-password -f /home/datatighub/backups/database-$current_date.sql datatighub
-
-zip -q -r /home/datatighub/backups/backup-$current_date.zip  /home/datatighub/backups/database-$current_date.sql  /home/datatighub/data
